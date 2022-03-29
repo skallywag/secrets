cb5f66e3ceef0098e09f0f69b6940ea3e924a3d1
Samuel Lee <lee.sam1492@gmail.com>
2022-03-29 00:17:48 -0600

    setting up folder structure/installing dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Shared/Components/Navbar/Navbar";
+import Home from "./Pages/Home/Home";
+import "./Styles/App.scss";
+import "./Styles/_reset.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/Home" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
